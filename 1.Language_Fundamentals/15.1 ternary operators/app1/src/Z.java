9ffd5640a6635f114e44e83fa75394b650d342a6
Prajwal267 <prajwalchola26@gmail.com>
2023-08-26 22:46:41 +0530

    second commit

---
+++ 1.Language_Fundamentals/15.1 ternary operators/app1/src/Z.java
@@ -0,0 +1,18 @@
+class Z
+{
+	public static void main(String[] args) 
+	{
+		int i = 200, j = 300;
+		int min;
+		if (i > j)
+		{
+			min = j;
+			System.out.println("min b/w " + i + " and " + j + " is " + min);
+		}
+		else
+		{
+			min = i;
+			System.out.println("min b/w " + i + " and " + j + " is " + min);
+		}
+	}
+}
