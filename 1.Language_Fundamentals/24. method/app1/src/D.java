cf9b50383a37a452d64fd81fbfe38bf606f5c878
Prajwal267 <prajwalchola26@gmail.com>
2023-09-20 22:58:08 +0530

    fourth commit

---
+++ 1.Language_Fundamentals/24. method/app1/src/D.java
@@ -0,0 +1,13 @@
+class D 
+{
+	public static void test()
+	{
+		System.out.println("from test");
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("from main");
+		D.test();
+		System.out.println("from mina end");
+	}
+}
